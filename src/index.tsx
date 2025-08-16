4447a03fd1f7c137c705199e4aa64299847747e7
Kimberly Moorhouse <kimberly.moorhouse@gmx.ch>
2025-08-17 00:44:52 +0200

    add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.scss';
-import App from './App';
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./theme";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router";
+import {ToDoPage} from "./pages/todo/ToDoPage";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -11,7 +12,13 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <App/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/calendar" replace />} />
+                    <Route path="/to-do" element={<ToDoPage />} />
+                    <Route path="/calendar" element={<ToDoPage />} />
+                </Routes>
+            </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>
 );
