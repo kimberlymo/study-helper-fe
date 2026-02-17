9aab63b439a294f728b4f4c53ec3cfd4bbd1d9a4
Kimberly Moorhouse <kimberly.moorhouse@gmx.ch>
2026-02-17 15:55:03 +0100

    improve routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.module.scss";
 import { ThemeProvider } from "@mui/material";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ToDoPage } from "./pages/todo/ToDoPage";
 import { theme } from "./theme";
-import {Overview} from "./pages/overview/Overview";
+import { Overview } from "./pages/overview/Overview";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Navigate to="/overview" replace />} />
 					<Route path="/to-do" element={<ToDoPage />} />
 					<Route path="/overview" element={<Overview />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
 );
